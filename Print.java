769ddce5ab2d4233a2b2f8281d79f8ce1bf7f606
Joshua-ace <ace.joshuabranch@gmail.com>
2025-03-17 19:15:34 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Print{
+ public static void main(String[]args){
+  Scanner scan = new Scanner(System.in);
+  
+  System.out.print("enter a integer number : ");
+  int a = scan.nextInt();
+  System.out.print("enter a string name : ");
+  String b = scan.next();
+  System.out.print("enter a double number :   ");
+   double c = scan.nextDouble();
+  
+  System.out.print("enter a string name : ");
+  String d = scan.next();
+
+
+  }
+  }
---
+++ Widenning.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ public class Widenning{
+  public static void main(String[]args){
+    
+	double d = 112.112;
+	float f = (float)d;
+	long li = (long)f;
+	int i = (int)li;
+	short si = (short)i;
+	byte b = (byte)si;
+	
+	System.out.println(d+" - "+ f+" - "+ li+" - "+ i+" - "+ si+" - "+b);
+	}
+	}
