9207bc2188f302d51400b8e8e9b509f21e956ebf
Joshua-ace <ace.joshuabranch@gmail.com>
2025-05-06 18:15:42 +0530

    Add files via upload

---
+++ Average1.java
@@ -0,0 +1,14 @@
+//write a java program to calculate the average value of array elements?
+
+class Average1{
+	public static void main(String...args){
+		int arr[] = {1,2,3,4,5,6,7};
+		int sum = 0;
+		int count = 0;
+		for(int i=0; i<arr.length; i++){
+		sum = sum+arr[i];
+		count++;
+		}
+		System.out.println("the avg of array elements is: "+(sum/count));
+		}
+	}
